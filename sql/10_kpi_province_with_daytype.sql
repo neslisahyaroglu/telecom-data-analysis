58c269deff09434f69f80a0e0fe4914d7da531e0
demetdevrik <demettdevrik@gmail.com>
2026-02-02 14:27:18 +0300

    10_kpi_province_with_daytype.sql
    -- Compares average or total traffic levels of provinces
     between weekdays and weekends to detect behavioral differences.

---
+++ sql/10_kpi_province_with_daytype.sql
@@ -0,0 +1,23 @@
+-- Purpose:
+-- Compares average or total traffic levels of provinces
+-- between weekdays and weekends to detect behavioral differences.
+--
+-- Source table:
+-- mi_to_provinces_all_days
+--
+-- KPI:
+-- Province Day Type Behavior
+
+SELECT
+  datetime,
+  date,
+  time,
+  cell_id,
+  province_name,
+  cell2Province,
+  Province2cell,
+  CASE
+    WHEN EXTRACT(DAYOFWEEK FROM date) IN (1, 7) THEN 'Weekend'
+    ELSE 'Weekday'
+  END AS day_type
+FROM `ndn-project-485520.telecom_italy_data.mi_to_provinces_all_days`;
